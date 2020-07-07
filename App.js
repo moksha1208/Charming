f4fd81edcf7926251283f69ed1f2f5d12d75e3ca
jhanvishah <jhanvishah26@gmail.com>
2020-07-07 20:39:09 +0530

    ss

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render () {
+        return (
+            <View>
+                <Text>Hey!!</Text>
+            </View>
+        )
+    }
+}
